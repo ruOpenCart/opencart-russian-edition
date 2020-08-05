29bd15f501647d8ff02a95051987bc0a05914374
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-08-05 09:07:26 +0300

    Обновил ссылки на форум при установке

--- upload/install/language/ru-ru/common/footer.php
+++ upload/install/language/ru-ru/common/footer.php
@@ -5,5 +5,5 @@ $_['text_documentation']   = 'Документация';
 $_['text_support']         = 'Форум поддержки';
 
 // RE
-$_['text_project']         = 'Домашняя страница сборки';
-$_['text_localization']    = 'Форум локализации';
+$_['text_project_re']      = 'Домашняя страница сборки';
+$_['text_support_re']      = 'Форум локализации';
